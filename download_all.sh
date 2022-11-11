54da095fc80e42b73f70373bddb61ef35da9c20f
jp112sdl <jp112sdl@icloud.com>
2022-11-11 18:23:47 +0100

    initial commit

---
+++ download_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Getting firmware list"
+output=`curl -s 'https://update.homematic.com/firmware/api/firmware/search/DEVICE' | sed s/'homematic.com.setDeviceFirmwareVersions('/''/ | sed s/');'/''/ |  jq -r '.[] | "\(.type)"'`
+
+echo "Downloading firmware files"
+for row in ${output}; do
+  curl -s -q -O -J -L  "https://ccu3-update.homematic.com/firmware/download?cmd=download&serial=0&product=${row}"
+done
+
+echo "Moving files into directories"
+for f in *gz; do
+  pref=`ls $f|awk -F'[-_]' {'print $1'}`
+  case $pref in
+    ([Hh][Mm]) pref="HM";;
+    ([Hh][Mm][Ii][Pp]) pref="HmIP";;
+    ([Hh][Mm][Ii][Pp][Ww]) pref="HmIPW";;
+    ([Ee][Ll][Vv]) pref="ELV";;
+  esac
+  [ ! -d $pref ] && mkdir $pref
+  mv $f $pref/
+done
