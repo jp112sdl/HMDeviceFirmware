e4aaa754db60eb35253c8b3eebdf12393240b348
jp112sdl <jp112sdl@icloud.com>
2022-12-01 10:35:02 +0100

    show download progress

--- download_all.sh
+++ download_all.sh
@@ -2,12 +2,14 @@
 
 echo "Getting firmware list"
 output=`curl -s 'https://update.homematic.com/firmware/api/firmware/search/DEVICE' | sed s/'homematic.com.setDeviceFirmwareVersions('/''/ | sed s/');'/''/ |  jq -r '.[] | "\(.type)"'`
-
-echo "Downloading firmware files"
+cnt=`wc -w <<< "$output"|xargs`
+i=0
 for row in ${output}; do
+  i=$((i+1))
+  echo -n -e "Downloading firmware file $i of $cnt (${row}).              \r"
   curl -s -q -O -J -L  "https://ccu3-update.homematic.com/firmware/download?cmd=download&serial=0&product=${row}"
 done
-
+echo ""
 echo "Moving files into directories"
 for f in *gz; do
   pref=`ls $f|awk -F'[-_]' {'print $1'}`
