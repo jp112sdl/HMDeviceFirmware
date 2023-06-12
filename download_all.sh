47d69ffcc47f494ed934ef1b1fca9aab680edfc2
Jérôme <jp112sdl@icloud.com>
2023-06-12 11:09:03 +0200

    Update download_all.sh

--- download_all.sh
+++ download_all.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-echo "Getting firmware list"
+rm -f run_*
+current_date=`date +%Y-%m-%d-%H-%M-%S`
+runfile="run_${current_date}.txt"
+touch ${runfile}
+
+ip=$(curl -s https://api.ipify.org)
+echo "External IP: $ip" | tee -a ${runfile}
+
+echo "Getting firmware list" | tee -a ${runfile}
 output=`curl -s 'https://update.homematic.com/firmware/api/firmware/search/DEVICE' | sed s/'homematic.com.setDeviceFirmwareVersions('/''/ | sed s/');'/''/ |  jq -r '.[] | "\(.type)"'|sort -u`
 cnt=`wc -w <<< "$output"|xargs`
 i=0
 for row in ${output}; do
   i=$((i+1))
-  echo -n -e "Downloading firmware file $i of $cnt (${row}).              \r"
+  echo -n -e "Downloading firmware file $i of $cnt (${row}).              \r" | tee -a ${runfile}
   curl -fsSLOJ  "https://ccu3-update.homematic.com/firmware/download?cmd=download&serial=0&product=${row}"
 done
-echo ""
-echo "Moving files into directories"
+echo "" | tee -a ${runfile}
+echo "Moving files into directories" | tee -a ${runfile}
 for f in *gz; do
   pref=`ls $f|awk -F'[-_]' {'print $1'}`
   case $pref in
@@ -22,5 +30,4 @@ for f in *gz; do
   [ ! -d $pref ] && mkdir $pref
   mv $f $pref/
 done
-current_date=`date +%Y-%m-%d`
-touch run_${current_date}.txt
+echo "Done." | tee -a ${runfile}
